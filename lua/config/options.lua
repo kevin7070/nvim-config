41f4d4fa5082e6bb41d04bdffeb0b154626774bc
kevin7070 <kevin.7070@gmail.com>
2026-06-17 13:20:06 -0400

    chore(config): disable unused language providers
    Perl/Ruby/Node/Python3 provider hosts are unused by LazyVim and our LSP-based stack; disabling them clears the corresponding :checkhealth warnings without installing extra packages.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable unused language providers (legacy remote-plugin hosts).
+-- LazyVim and our React/Django stack rely on LSP, not these. Silences the
+-- corresponding :checkhealth warnings without installing anything.
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
